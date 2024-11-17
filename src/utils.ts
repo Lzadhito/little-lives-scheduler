7de85f8878b708cb5d7149707fedf0b96c5c0a88
lzadhito <lzadhito@gmail.com>
2024-11-17 11:53:56 +0700

    chore: developer note

--- src/utils.ts
+++ src/utils.ts
@@ -2,6 +2,9 @@ import { Availability, DaySetting, Day } from "./types";
 
 export const deepClone = (obj: Object) => JSON.parse(JSON.stringify(obj));
 
+// Note: Im using ChatGPT for only (and only) this perticular function, I'm sorry that I had to do it
+// I'm being nervous for the tight deadline, you are welcome to judge my action. I'm really sorry
+// I also dont have the time to refactor this to use my own
 export const generateTime = (interval: number): string[] => {
   const startHour = 7;
   const endHour = 19;
